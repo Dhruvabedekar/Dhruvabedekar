f8b658a5fa44dfa8a0ce731eb8af045f5d371701
Dhruvabedekar <89086890+Dhruvabedekar@users.noreply.github.com>
2023-10-19 12:24:34 +0530

    Add files via upload

---
+++ print A.c
@@ -0,0 +1,22 @@
+//A
+#include <stdio.h>
+int main(){
+	int r,s,n,i;
+
+	for(r=1;r<=5;r++){
+		if(r==1||r==3){
+			for(s=1;s<=5;s++){
+			printf("*");
+		}
+		}else{
+			printf("*");
+			for(i=0;i<3;i++){
+			printf(" ");
+		}
+		printf("*");
+		}
+	
+		printf("\n");
+	}
+	return 0;
+}
